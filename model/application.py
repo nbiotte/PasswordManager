fee30a315164afe5f5660be5180d2d69a4630d57
nbiotte <nathanbiotte@gmail.com>
2021-08-29 12:21:23 +0200

    modification main password

--- model/application.py
+++ model/application.py
@@ -154,10 +154,11 @@ class MainApplication(tk.Frame):
         self.createWidgetsPasswords()
 
     def modifyMainPassword(self):
-        secondaryWindow = tk.Tk()
+        secondaryWindow = tk.Toplevel()
         secondaryWindow.geometry("200x200")
         secondaryWindow.resizable(width=False, height=True)
         secondaryWindow.title('Changer le mot de passe principal')
+        secondaryWindow.grab_set()
         app = ChangeMainPasswordApplication(master=secondaryWindow)
         app.mainloop()
 
@@ -165,23 +166,31 @@ class ChangeMainPasswordApplication(tk.Frame):
     def __init__(self, master=None):
         self.window = master
         super().__init__(self.window)
-        self.pack()
         self.file = file.File()
         self.encrypter = encrypter.Encrypter()
+        self.pack()
         self.createWidgets()
 
     def createWidgets(self):
         jsonDictionnary = self.file.getJson()
 
-        self.password = tk.StringVar()
-        self.password.set(self.encrypter.decrypt(jsonDictionnary["MainPassword"]))
-        entryPassword = tk.Entry(self.window, textvariable=self.password)
+        password = tk.StringVar()
+        # password.set(self.encrypter.decrypt(jsonDictionnary["MainPassword"]))
+        password.set('test')
+        entryPassword = tk.Entry(self.window, textvariable=password)
         entryPassword.pack(side=tk.TOP, padx=5, pady=5)
 
         btModifyMainPassword = tk.Button(self.window, text="Modifier mot de passe principal",
-                                         command=self.modifyPassword)
+                                         command=lambda: self.modifyPassword(password))
         btModifyMainPassword.pack(side=tk.BOTTOM)
 
-    def modifyPassword(self):
-        print(self.password.get())
-        pass
+    def modifyPassword(self, password):
+        print(password.get())
+        jsonDictionnary = self.file.getJson()
+
+        newPassword = {"MainPassword": self.encrypter.encrypt(password.get())}
+
+        jsonDictionnary.update(newPassword)
+
+        self.file.setJson(jsonDictionnary)
+        self.window.destroy()
